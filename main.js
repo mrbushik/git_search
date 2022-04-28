ab268ce1cec55d9956ff6c817b4fbf89f0fc5f88
Nikita Bushuev <mrbushik1@gmail.com>
2022-04-28 15:37:55 +0300

    output one repository

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+'use strict'
 const API_URL = "https://api.github.com/search/repositories?q=";
 
 const main = document.getElementById('main');
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const repositoriesCount = document.querySelector('.repository-count')
+
+
 search.addEventListener('input', () => {
   let searchText = search.value;
   getUser(searchText);
@@ -11,23 +14,24 @@ search.addEventListener('input', () => {
 
 })
 
-const getUser = async (username) => {
-  const repository = await fetch(API_URL + username)
+
+const getUser = async (repositoryName) => {
+  const repository = await fetch(API_URL + repositoryName)
   const repositoryData = await repository.json();
   createUserCard(repositoryData)
 }
 
 
-const createUserCard = (user) => {
+const createUserCard = (repositoryValue) => {
   const cardHTML = `
   <div class="card">
   <div>
 
   </div>
   <div class="user-info">
-  <h2>Имя ${user.items[0].name}</h2>
- <p>Просмотры ${user.items[0].watchers}</p>
- <p>Ссылка <a href="${user.items[0].html_url}"> ${user.items[0].html_url}</a></p>
+  <h2>Имя ${repositoryValue.items[0].name}</h2>
+ <p>Просмотры ${repositoryValue.items[0].watchers}</p>
+ <p>Ссылка <a href="${repositoryValue.items[0].html_url}"> ${repositoryValue.items[0].html_url}</a></p>
   </div>
   </div>`
   main.innerHTML = cardHTML;
