bda536d06f828f141bb0925c7dbc3b0ebc432a95
Nikita Bushuev <mrbushik1@gmail.com>
2022-04-28 18:28:40 +0300

    lost of repositories is displayed

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
-'use strict'
+'use strict';
 const API_URL = "https://api.github.com/search/repositories?q=";
 
 const main = document.getElementById('main');
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const repositoriesCount = document.querySelector('.repository-count')
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const repositoriesCountText = document.querySelector('.search-resault_count');
+
+let count = 0;
 
 
 search.addEventListener('input', () => {
   let searchText = search.value;
   getUser(searchText);
-
-
-})
+});
 
 
 const getUser = async (repositoryName) => {
-  const repository = await fetch(API_URL + repositoryName)
+  const repository = await fetch(API_URL + repositoryName);
   const repositoryData = await repository.json();
-  createUserCard(repositoryData)
-}
+  let repositoriesCount = repositoryData.total_count;
+  repositoriesCountText.textContent = repositoriesCount;
+  createUserCards(repositoriesCount, repositoryData);
+};
 
+const createUserCards = (repositoriesCount, repositoryValue) => {
+  let i = 0;
 
-const createUserCard = (repositoryValue) => {
-  const cardHTML = `
-  <div class="card">
-  <div>
+  if (repositoriesCount > 10) {
 
-  </div>
-  <div class="user-info">
-  <h2>Имя ${repositoryValue.items[0].name}</h2>
- <p>Просмотры ${repositoryValue.items[0].watchers}</p>
- <p>Ссылка <a href="${repositoryValue.items[0].html_url}"> ${repositoryValue.items[0].html_url}</a></p>
-  </div>
-  </div>`
-  main.innerHTML = cardHTML;
-}
+    while (i < 10) {
+      createElements(repositoryValue, i);
+      i++;
+    }
+  }
+};
+
+const createElements = (repositoryValue, i = 0) => {
+  let searchElement = document.createElement('div');
+  searchElement.innerHTML = `<div class="user-info">
+     <h2>Имя: ${repositoryValue.items[i].name}</h2>
+    <p>Просмотры ${repositoryValue.items[i].watchers}</p>
+    <p>Ссылка <a href="${repositoryValue.items[i].html_url}"> ${repositoryValue.items[i].html_url}</a></p>
+     </div>`;
+  main.append(searchElement);
+};
